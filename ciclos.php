928f4289cc3e9197befbdadf1cbb93b1461ff1ea
ivan jara <103223959+ivjaram@users.noreply.github.com>
2022-05-28 10:07:22 -0400

    ciclos for y while-php

---
+++ ciclos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+    <title>ciclos for y while</title>
+</head>
+<body>
+    <?php
+    $num = 3;
+    for ($i=1; $i <=10; $i++) { 
+        $total =$i*$num;
+        echo " <br />3 x " .$i." = ".$total;
+        
+    }
+
+
+?>
+</body>
+</html>
